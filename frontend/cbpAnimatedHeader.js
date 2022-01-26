87ed834dc4fabd6412e88761a6133723eb674bb9
Alejandro <alejandro1998hernandez@gmail.com>
2022-01-26 19:27:44 +0100

    animación del navbar done

--- frontend/cbpAnimatedHeader.js
+++ frontend/cbpAnimatedHeader.js
@@ -1,44 +1,32 @@
-/**
- * cbpAnimatedHeader.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-var cbpAnimatedHeader = (function() {
-
-	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-default' ),
-		didScroll = false,
-		changeHeaderOn = 100;
-
-	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
-	}
-
-	function scrollPage() {
-		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
-		didScroll = false;
-	}
-
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
-
-	init();
-
-})();
+$(document).ready(function() {
+  var idSections = ["#historia", "#agrupament", "#proposta", "#branques", "#calendari", "#contacta"]
+  var posSections = [$('#historia').position().top,
+  $('#agrupament').position().top,
+  $('#proposta').position().top,
+  $('#branques').position().top,
+  $('#calendari').position().top,
+  $('#contacta').position().top]
+  $(window).scroll(function() {
+    if (window.pageYOffset >= 500) {
+      $('.navbar-default').addClass('navbar-shrink');
+    } else {
+      $('.navbar-default').removeClass('navbar-shrink');
+    }
+    for (var i = 0; i < posSections.length; i++) {
+      if(window.pageYOffset >= posSections[i]
+          && (window.pageYOffset < posSections[i+1] || i == 5)
+           && (window.pageYOffset > posSections[i-1] || i == 0)){
+        $('a[href="'+idSections[i]+'"]').parent().addClass('active')
+      } else {
+        $('a[href="'+idSections[i]+'"]').parent().removeClass('active')
+      }
+    }
+  });
+  $("a.page-scroll").click(function(e) {
+    var href = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(href).offset().top
+    }, 2000);
+    e.preventDefault()
+  });
+});
