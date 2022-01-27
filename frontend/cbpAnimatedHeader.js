645ca2530164ec616cfc1ab7196290d852400190
Alejandro <alejandro1998hernandez@gmail.com>
2022-01-27 01:50:38 +0100

    reajuste acciones y diseño navbar

--- frontend/cbpAnimatedHeader.js
+++ frontend/cbpAnimatedHeader.js
@@ -1,32 +1,52 @@
 $(document).ready(function() {
   var idSections = ["#historia", "#agrupament", "#proposta", "#branques", "#calendari", "#contacta"]
   var posSections = [$('#historia').position().top,
-  $('#agrupament').position().top,
-  $('#proposta').position().top,
-  $('#branques').position().top,
-  $('#calendari').position().top,
-  $('#contacta').position().top]
+    $('#agrupament').position().top,
+    $('#proposta').position().top,
+    $('#branques').position().top,
+    $('#calendari').position().top,
+    $('#contacta').position().top
+  ]
+  animarNav(idSections, posSections)
   $(window).scroll(function() {
-    if (window.pageYOffset >= 500) {
-      $('.navbar-default').addClass('navbar-shrink');
-    } else {
-      $('.navbar-default').removeClass('navbar-shrink');
-    }
-    for (var i = 0; i < posSections.length; i++) {
-      if(window.pageYOffset >= posSections[i]
-          && (window.pageYOffset < posSections[i+1] || i == 5)
-           && (window.pageYOffset > posSections[i-1] || i == 0)){
-        $('a[href="'+idSections[i]+'"]').parent().addClass('active')
-      } else {
-        $('a[href="'+idSections[i]+'"]').parent().removeClass('active')
-      }
-    }
+    animarNav(idSections, posSections)
+  });
+  $(window).resize(function() {
+    animarNav(idSections, posSections, window)
   });
   $("a.page-scroll").click(function(e) {
     var href = $(this).attr('href');
+    let nav
+    if (screen.width >= 1200 && href != "#page-top"){
+      nav = $('nav').innerHeight() - $('#menu').innerHeight()
+      $('button.navbar-toggler').click()
+    }
+     if(href === "#page-top"){
+      nav = 0
+    }
+    if (screen.width <= 1200) {
+      nav = $('nav').innerHeight()
+    }
     $('html,body').animate({
-      scrollTop: $(href).offset().top
+      scrollTop: $(href).offset().top - nav
     }, 2000);
     e.preventDefault()
   });
 });
+
+function animarNav(idSections, posSections) {
+  if (window.pageYOffset >= posSections[0] - $('nav').innerHeight()) {
+    $('.navbar-default').addClass('navbar-shrink');
+  } else {
+    $('.navbar-default').removeClass('navbar-shrink');
+  }
+  for (var i = 0; i < posSections.length; i++) {
+    if (window.pageYOffset >= posSections[i] - $('nav').innerHeight() &&
+      (window.pageYOffset < posSections[i + 1] - $('nav').innerHeight() || i == 5) &&
+      (window.pageYOffset > posSections[i - 1] - $('nav').innerHeight() || i == 0)) {
+      $('a[href="' + idSections[i] + '"]').parent().addClass('active')
+    } else {
+      $('a[href="' + idSections[i] + '"]').parent().removeClass('active')
+    }
+  }
+}
