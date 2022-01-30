595dbf2d6fd37d92497ebde7f6352d649d48e77c
Alejandro <alejandro1998hernandez@gmail.com>
2022-01-30 12:12:55 +0100

    ajuste nav

--- frontend/cbpAnimatedHeader.js
+++ frontend/cbpAnimatedHeader.js
@@ -1,30 +1,21 @@
 $(document).ready(function() {
-  var idSections = ["#historia", "#agrupament", "#proposta", "#branques", "#calendari", "#contacta"]
-  var posSections = [$('#historia').position().top,
-    $('#agrupament').position().top,
-    $('#proposta').position().top,
-    $('#branques').position().top,
-    $('#calendari').position().top,
-    $('#contacta').position().top
-  ]
-  animarNav(idSections, posSections)
+  const idSections = ["#historia", "#agrupament", "#proposta", "#branques", "#calendari", "#contacta"]
+  animarNav(idSections)
   $(window).scroll(function() {
-    animarNav(idSections, posSections)
+    animarNav(idSections)
   });
   $(window).resize(function() {
-    animarNav(idSections, posSections, window)
+    animarNav(idSections)
   });
   $("a.page-scroll").click(function(e) {
     var href = $(this).attr('href');
     let nav
-    if (screen.width >= 1200 && href != "#page-top"){
+    if(href === "#page-top"){
+     nav = 0
+   } else if (screen.width <= 1200){
       nav = $('nav').innerHeight() - $('#menu').innerHeight()
       $('button.navbar-toggler').click()
-    }
-     if(href === "#page-top"){
-      nav = 0
-    }
-    if (screen.width <= 1200) {
+    } else {
       nav = $('nav').innerHeight()
     }
     $('html,body').animate({
@@ -34,19 +25,31 @@ $(document).ready(function() {
   });
 });
 
-function animarNav(idSections, posSections) {
+function animarNav(idSections) {
+  var posSections = getPosSec()
   if (window.pageYOffset >= posSections[0] - $('nav').innerHeight()) {
     $('.navbar-default').addClass('navbar-shrink');
   } else {
     $('.navbar-default').removeClass('navbar-shrink');
   }
   for (var i = 0; i < posSections.length; i++) {
-    if (window.pageYOffset >= posSections[i] - $('nav').innerHeight() &&
-      (window.pageYOffset < posSections[i + 1] - $('nav').innerHeight() || i == 5) &&
-      (window.pageYOffset > posSections[i - 1] - $('nav').innerHeight() || i == 0)) {
+    if (window.pageYOffset >= posSections[i] - $('nav').innerHeight()
+     && (window.pageYOffset < posSections[i + 1] - $('nav').innerHeight() || i == 5)
+     && (window.pageYOffset > posSections[i - 1] - $('nav').innerHeight() || i == 0)) {
       $('a[href="' + idSections[i] + '"]').parent().addClass('active')
     } else {
       $('a[href="' + idSections[i] + '"]').parent().removeClass('active')
     }
   }
+}
+
+function getPosSec(){
+  var posSections = [$('#historia').position().top,
+    $('#agrupament').position().top,
+    $('#proposta').position().top,
+    $('#branques').position().top,
+    $('#calendari').position().top,
+    $('#contacta').position().top
+  ]
+  return posSections
 }
